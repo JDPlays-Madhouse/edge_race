19412c0320cd6c8a8a1cab01b1736c99382bb069
Zack Hankin <admin@hankin.io>
2025-12-20 21:23:49 +1100

    void added correctly

--- prototypes/void_tiles.lua
+++ prototypes/void_tiles.lua
@@ -1,10 +1,86 @@
 --- Use https://lua-api.factorio.com/latest/prototypes/CharacterPrototype.html#flying_collision_mask
 
 -- TODO: Add a space age check.
+local directions_of_travel = require("__edge_race__.utility.lists.direction"); ---@type DirectionOfTravel
+local collision_mask_util = require("__core__.lualib.collision-mask-util");
+local collision_mask_default = require("__core__.lualib.collision-mask-defaults");
+local character_collision_mask = collision_mask_default["character/flying"];
 
-local void_tile_collision_mask = table.deepcopy(data.raw["tile"]["out_of_map"].collision_mask);
-local my_void_tile = table.deepcopy(data.raw["tile"]["empty_space"]);
+-- local void_tile_collision_mask = table.deepcopy(data.raw["tile"]["out-of-map"].collision_mask);
+local my_void_tile = table.deepcopy(data.raw["tile"]["out-of-map"]);
 
-my_void_tile.name = "race_void_tile";
-my_void_tile.collision_mask = void_tile_collision_mask;
-data:extend({ my_void_tile })
+
+---Noise function for a given direction
+---@return string
+function wall_noise_function()
+    -- local offset = settings.startup["wall-offset-distance"].value; ---@cast offset uint
+    local direction = directions_of_travel.direction_with_settings();
+    -- direction = directions_of_travel.direction(defines.direction.west);
+    -- direction.cardinal = defines.direction.west;
+
+    local noise;
+    if direction.cardinal == defines.direction.north then
+        noise = "y<(" .. direction.starting_back_wall .. ")";
+    elseif direction.cardinal == defines.direction.south then
+        noise = "y>(" .. direction.starting_back_wall .. ")";
+    elseif direction.cardinal == defines.direction.west then
+        noise = "x>(" .. direction.starting_back_wall .. ")";
+    elseif direction.cardinal == defines.direction.east then
+        noise = "x<(" .. direction.starting_back_wall .. ")";
+    end
+
+    return noise
+end
+
+-- void_tile_collision_mask.layers["race-void-tile"] = true
+my_void_tile.name = "race-void-tile";
+my_void_tile.layer = 1;
+-- my_void_tile.collision_mask = {
+--     layers = {
+--         -- ground_tile=true,
+--         -- water_tile=true,
+--         -- resource=true,
+--         floor = true,
+--         item = true,
+--         object = true,
+--         player = true,
+--         doodad = true,
+--         out_of_map = true
+--     }
+-- };
+my_void_tile.autoplace = {
+    -- probability_expression = wall_noise_function(),
+    -- richness_expression = wall_noise_function(),
+
+    probability_expression = "(x>100)*100000+(x<=100)*-100000",
+    default_enabled = false,
+    order = "zzzzz"
+}
+data:extend({
+    my_void_tile,
+}
+)
+
+for name, planet in pairs(data.raw["planet"]) do
+    planet = table.deepcopy(planet);
+
+    local map_gen_settings = planet.map_gen_settings;
+
+    if map_gen_settings == nil then
+        map_gen_settings = {};
+    end
+
+    if map_gen_settings.autoplace_settings ~= nil then
+        map_gen_settings.autoplace_settings["tile"].settings["race-void-tile"] = {
+        }
+    else
+        map_gen_settings.autoplace_settings = {
+            ["tile"] = {
+                settings = {
+                    ["race-void-tile"] = {}
+                }
+            }
+        }
+    end
+    data.raw["planet"][name].map_gen_settings = map_gen_settings
+end
--- utility/lists/direction.lua
+++ utility/lists/direction.lua
@@ -2,39 +2,39 @@ local directions_of_travel = {} ---@class DirectionOfTravel
 
 ---@class Direction
 ---@field name defines.direction
----@field race_width uint
 ---@field map_gen_settings MapGenSettings
----@field starting_back_wall uint
+---@field starting_back_wall int
+---@field cardinal CardinalDirection
 
----@class BackWallPosition
----@field offset uint
----@field orientation RealOrientation
-
+---@alias CardinalDirection defines.direction.north|defines.direction.east|defines.direction.west|defines.direction.south
 
 --- Generates all the race parameters.
---- @param direction_setting defines.direction.north|defines.direction.east|defines.direction.west|defines.direction.south
+--- @param direction_setting CardinalDirection
 ---@return Direction Direction Settings for the race.
 directions_of_travel.direction = function(direction_setting)
     local race_width = 500;
 
-    local direction = { race_width = race_width, map_gen_settings = { width = 2000000, height = 2000000, } };
+    local direction = {
+        name = direction_setting,
+        race_width = race_width,
+        map_gen_settings = { width = 2000000, height = 2000000, },
+        starting_back_wall = 100,
+        cardinal =
+            direction_setting
+    };
 
     if direction_setting == defines.direction.north then
         direction.map_gen_settings.width = race_width
         direction.starting_back_wall = -race_width
-        direction.name = defines.direction.north
     elseif direction_setting == defines.direction.west then
         direction.map_gen_settings.height = race_width
         direction.starting_back_wall = race_width
-        direction.name = defines.direction.west
     elseif direction_setting == defines.direction.south then
         direction.map_gen_settings.width = race_width
         direction.starting_back_wall = race_width
-        direction.name = defines.direction.south
     else
         direction.map_gen_settings.height = race_width
         direction.starting_back_wall = -race_width
-        direction.name = defines.direction.east
     end --[[@cast direction Direction]]
 
     return direction;
@@ -46,6 +46,58 @@ end
 ---@return MapGenSettings
 directions_of_travel.map_gen_settings = function(direction_setting)
     return directions_of_travel.direction(direction_setting).map_gen_settings;
+end
+
+
+--- Map input to a valid CardinalDirection
+---@param input string
+---@return CardinalDirection|nil
+directions_of_travel.map_direction = function(input)
+    if input == "up" then
+        return defines.direction.north
+    elseif input == "left" then
+        return defines.direction.west
+    elseif input == "down" then
+        return defines.direction.south
+    elseif input == "right" then
+        return defines.direction.east
+    else
+        return nil
+    end
+end
+--- Generates all the race parameters.
+---@return Direction Direction Settings for the race.
+directions_of_travel.direction_with_settings = function()
+    local race_width = 500;
+    local offset = settings.startup["wall-offset-distance"].value; ---@cast offset int
+    local direction_setting = directions_of_travel.map_direction(settings.startup["direction-of-travel"].value);
+
+    local direction = {
+        race_width = race_width,
+        map_gen_settings = { width = 2000000, height = 2000000, },
+        cardinal =
+            direction_setting
+    };
+
+    if direction_setting == defines.direction.north then
+        direction.map_gen_settings.width = race_width
+        direction.starting_back_wall = -offset
+        direction.name = defines.direction.north
+    elseif direction_setting == defines.direction.west then
+        direction.map_gen_settings.height = race_width
+        direction.starting_back_wall = offset
+        direction.name = defines.direction.west
+    elseif direction_setting == defines.direction.south then
+        direction.map_gen_settings.width = race_width
+        direction.starting_back_wall = offset
+        direction.name = defines.direction.south
+    else
+        direction.map_gen_settings.height = race_width
+        direction.starting_back_wall = -offset
+        direction.name = defines.direction.east
+    end --[[@cast direction Direction]]
+
+    return direction;
 end
 
 return directions_of_travel;
